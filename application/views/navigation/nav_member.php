6de20a916eee4f9eb811776b1da5a0933b707247
Clarrence <clarrence.iformatlogic@gmail.com>
2018-03-09 13:20:18 +0800

    fixed nav_members items

--- application/views/navigation/nav_member.php
+++ application/views/navigation/nav_member.php
@@ -1,27 +1,24 @@
 <nav class="sidebar-nav">
     <ul id="sidebarnav">
-        <li>
-            <a href="<?= base_url('about'); ?>" aria-expanded="false"><span class="hide-menu">About</span></a>
-        </li>
-        <li>
+        <li class="pt-1">
             <a class="has-arrow waves-effect waves-dark" href="<?php echo base_url('/dashboard') ?>" aria-expanded="false">
                 <span class="hide-menu">Dashboard</span>
             </a>
         </li>
 
         <li>
-            <a href="<?= base_url('jobs'); ?>">Job Bank </a>
-            <!-- <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false">
+            <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false">
                 <span class="hide-menu">Job Bank</span><i class="mdi mdi-chevron-down"></i>
             </a>
             <ul>
                 <li><a href="<?= base_url('jobs'); ?>">Browse Jobs </a></li>
+                <li><a href="<?= base_url('jobs/posted'); ?>">My Posted Job</a></li>
                 <li><a href="<?= base_url('jobs/invitations'); ?>">Job Invitations</a></li>
                 <li><a href="<?= base_url('jobs/bid-history') ?>">My Bids</a></li>
                 <li><a href="<?= base_url('jobs/previous-project') ?>"> Previous Project </a></li>
-            </ul> -->
+            </ul>
         </li>
-        <li>
+        <li class="pt-1">
             <a href="<?= base_url('jobs/create'); ?>">Post Job</a>
             <!-- <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><span class="hide-menu">Manage</span><i class="mdi mdi-chevron-down"></i></a>
             <ul>
@@ -30,12 +27,10 @@
                 <li><a href="<?= base_url('members'); ?>">Browse Experts</a></li>
             </ul> -->
         </li>
-        <li>
-            <a class="waves-effect waves-dark" href="/pricing" aria-expanded="false"><span class="hide-menu">Pricing</span></a>
-            <!-- <a class="has-arrow waves-effect waves-dark" href="<?php echo base_url('/watch-list') ?>" aria-expanded="false">
+        <li class="pt-1">
+            <a class="has-arrow waves-effect waves-dark" href="<?php echo base_url('/watch-list') ?>" aria-expanded="false">
                 <span class="hide-menu">Watchlist</span>
-                <i class="mdi mdi-chevron-down hidden-xs"></i>
-            </a> -->
+            </a>
         </li>
     </ul>
 </nav>
